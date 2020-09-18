22290cb229117d9cab02885a8b638cd5cd636728
akhidev <59053561+akhidev@users.noreply.github.com>
2020-09-18 20:25:19 +0100

    Updated for tomcat compatabiliy

--- src/main/java/com/atz/pmd/PmdApplication.java
+++ src/main/java/com/atz/pmd/PmdApplication.java
@@ -2,9 +2,10 @@ package com.atz.pmd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PmdApplication {
+public class PmdApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PmdApplication.class, args);
